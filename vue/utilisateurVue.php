f7c4fc3fcb5c373f33fdeb1ac216aa1783a9b399
Grand-Mamy <Grand-Mamy@192.168.1.2>
2016-01-09 00:19:10 +0100

    4 vues + connect et deconnect

---
+++ vue/utilisateurVue.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Visitor</title>
+    </head>
+    <body>
+       <h1>Salut à toi : Simple visitor</h1>
+        <p><a href="?connect">Se connecter</a></p>
+    </body>
+</html>
